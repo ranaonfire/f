bab6b584c0807ab780a5a1557b6e6b5f3982ac21
ranaonfire <84216882+ranaonfire@users.noreply.github.com>
2021-06-09 08:59:34 +0500

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,208 @@
+# coding=utf-8
+# write by rana aahil
+#orginal coder - AAHIL ()
+try:
+    import concurrent.futures
+except ImportError:
+    print '\n [-] Modul Futures Now Install!\n'
+    os.system('pip install futures' if os.name == 'nt' else 'pip2 install futures')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print '\n [-] Modul ruby Now Install!\n'
+    os.system('pkg install ruby' if os.name == 'nt' else 'pkg install ruby')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print '\n [-] Modul lolcat Now Install!\n'
+    os.system('gem install lolcat' if os.name == 'nt' else 'gem install lolcat')
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    print '\n [-] Modul bs4 Now Install!\n'
+    os.system('pip install bs4' if os.name == 'nt' else 'pip2 install bs4')
+import requests, bs4, sys, os, subprocess, random, time, re, json
+import base64
+from concurrent.futures import ThreadPoolExecutor as YayanGanteng
+from datetime import datetime
+from time import sleep
+ct = datetime.now()
+n = ct.month
+bulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'Nopember', 'Desember']
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+op = bulan[nTemp]
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+try:
+    import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, uuid, cookielib, getpass, mechanize, requests
+    from multiprocessing.pool import ThreadPool
+except ImportError:
+    os.system('pip2 install requests')
+    os.system('pkg install nodejs')
+    os.system('python2 Aahil')
+
+os.system('git pull')
+os.system('clear')
+w = '\x1b[1;97m'
+g = '\x1b[1;32m'
+red = '\x1b[1;97m'
+wrong = '\x1b[1;97m[\x1b[1;97m!\x1b[1;97m]'
+wg = '\x1b[1;97m{\x1b[1;32m\xe2\x80\xa2\x1b[1;97m]'
+bd = random.randint(20000000.0, 30000000.0)
+sim = random.randint(20000.0, 40000.0)
+header = {'x-fb-connection-bandwidth': repr(bd), 'x-fb-sim-hni': repr(sim), 'x-fb-net-hni': repr(sim), 'x-fb-connection-quality': 'EXCELLENT', 'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA', 'user-agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]', 'content-type': 'application/x-www-form-urlencoded', 'x-fb-http-engine': 'Liger'}
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+try:
+    os.mkdir('/sdcard/RANA')
+except OSError:
+    pass
+
+try:
+    os.mkdir('/sdcard/ids/RANA_ids')
+except OSError:
+    pass
+
+def rsa(word):
+    lix = [
+     '\x1b[1;91mR', '\x1b[1;92mS', '\x1b[1;93mA']
+    for i in range(5):
+        for x in range(len(lix)):
+            sys.stdout.write(('\r{}{}').format(str(word), lix[x]))
+            time.sleep(00.5)
+            sys.stdout.flush()
+
+def load(word):
+    lix = [
+     '/', '-', '╲', '|']
+    for i in range(5):
+        for x in range(len(lix)):
+            sys.stdout.write(('\r{}{}').format(str(word), lix[x]))
+            time.sleep(00.1)
+            sys.stdout.flush()
+            
+def loda(word):
+    lix = [
+     '\x1b[1;91m×', '\x1b[1;92m', '\x1b[1;92m√']
+    for i in range(5):
+        for x in range(len(lix)):
+            sys.stdout.write(('\r{}{}').format(str(word), lix[x]))
+            time.sleep(00.3)
+            sys.stdout.flush()
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0000000.0001)
+
+def info_tools():
+    os.system('clear')
+    print info
+    memew = raw_input(w + '[' + g + '?' + w + '] Press enter to back! ')
+    menu()
+
+os.system('git pull')
+from requests.exceptions import ConnectionError
+bd = random.randint(20000000.0, 30000000.0)
+sim = random.randint(20000.0, 40000.0)
+header = {'x-fb-connection-bandwidth': repr(bd), 'x-fb-sim-hni': repr(sim), 'x-fb-net-hni': repr(sim), 'x-fb-connection-quality': 'EXCELLENT', 'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA', 'user-agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]', 'content-type': 'application/x-www-form-urlencoded', 'x-fb-http-engine': 'Liger'}
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+os.system('rm -rf .txt')
+for n in range(5000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+from requests.exceptions import ConnectionError
+bd = random.randint(20000000.0, 30000000.0)
+sim = random.randint(20000.0, 40000.0)
+header = {'x-fb-connection-bandwidth': repr(bd), 'x-fb-sim-hni': repr(sim), 'x-fb-net-hni': repr(sim), 'x-fb-connection-quality': 'EXCELLENT', 'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA', 'user-agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]', 'content-type': 'application/x-www-form-urlencoded', 'x-fb-http-engine': 'Liger'}
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+#logo by Aahil
+
+
+def logo():
+	os.system('echo -e "\td8888b. .d8888.  .d8b.  \n\t88   8D 88   YP d8   8b \n\t88oobY   8bo.   88ooo88 \n\t88 8b      Y8b. 88~~~88 \n\t88  88. db   8D 88   88 \n\t88   YD  8888Y  YP   YP \n\t┌─────────────────────┐\n\t│    RSA PROGRAMER    │\n\t└─────────────────────┘ " | lolcat -a -d 20 -s 10000 \n')
+
+
+
+
+
+def log():
+	os.system('echo -e " \n          ___  _   _ ___  ____ ____ ____ \n          |__]  \_/  |__] |__| [__  [__  \n          |__]   |   |    |  | ___] ___] " | lolcat  -a -d 30 -s 580\n')
+	
+	
+def sameer():
+    os.system('clear')
+    logo()
+    print("")
+    print("")
+    print("")
+    print("")
+    os.system('echo -e "-----------------------------------------------"  | lolcat  -a -d 20 -s 10000')
+    print("")
+    os.system('echo -e "              INSTALING BYPASS"  | lolcat  -a -d 20 -s 10000')
+    print("")
+    os.system('echo -e "-----------------------------------------------"  | lolcat  -a -d 20 -s 10000')
+    print("")
+    print("")
+    loda('              Cracking Security.. ')
+    print("")
+    print("")
+    time.sleep(5)
+    bahi()
+    
+def bahi():
+    os.system ('clear')
+    log()
+    print("")
+    print("")
+    print("")
+    print("")
+    os.system('echo -e "-----------------------------------------------"  | lolcat  -a -d 20 -s 10000')
+    os.system('echo -e "                EXECUTE SCRIPTS"  | lolcat  -a -d 20 -s 10000')
+    os.system('echo -e "-----------------------------------------------"  | lolcat  -a -d 20 -s 10000')
+    print("")
+    print("")
+    os.system('echo -e "           MORE SCRIPTS COMNING SOON"  | lolcat  -a -d 20 -s 10000')
+    os.system('echo -e "-----------------------------------------------"  | lolcat  -a -d 20 -s 10000')
+    os.system('echo -e "            First Connect Localhost"  | lolcat  -a -d 20 -s 10000')
+    os.system('echo -e "-----------------------------------------------"  | lolcat  -a -d 20 -s 10000')
+    print("")
+    os.system('echo -e "[1] Connect Localhost"  | lolcat  -a -d 20 -s 10000')
+    os.system('echo -e "[2] Launch Boss Commands Free"  | lolcat  -a -d 20 -s 10000')
+    os.system('echo -e "[3] Launch Alex Commands Free"  | lolcat  -a -d 20 -s 10000')
+    os.system('echo -e "[4] Launch James Commands Free"  | lolcat  -a -d 20 -s 10000')
+    os.system('echo -e "[5] Launch Coder Commands Free"  | lolcat  -a -d 20 -s 10000')
+    print("")
+    os.system('echo -e "-----------------------------------------------"  | lolcat  -a -d 20 -s 10000')
+    print("")
+    op()
+    
+def op():
+    a = raw_input('Choose Option :')
+    if a == '1':
+        os.system ('cd A')
+        os.system('python2 b.py')
+    
+sameer()
